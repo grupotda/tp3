4280df6405a7238630cf6bca91c04c4e4155ad86
MarkoVrljicak <marcos.vrljicak@gmail.com>
2016-12-01 21:33:30 -0300

    tsp velocidad

--- tsproblem/aprox_tsp_test.py
+++ tsproblem/aprox_tsp_test.py
@@ -1,16 +1,24 @@
 from aprox_tsp import aprox_tsp
+import timeit
 
 if __name__ == "__main__":
     # Estas son solo para ver que funciona, hay que hacer otras
-    fri26 = aprox_tsp("matrices/fri26.m")
-    print "\nfri26"
-    for edge in fri26.iter_edges():
-        print edge
-    gr17 = aprox_tsp("matrices/gr17.m")
-    print "\ngr17"
-    for edge in gr17.iter_edges():
-        print edge
+    start_time = timeit.default_timer()
     p01 = aprox_tsp("matrices/p01.m")
-    print "\np01"
-    for edge in p01.iter_edges():
-        print edge
+    res = timeit.default_timer() - start_time
+    print "15 nodes:", res, "seconds"
+
+    start_time = timeit.default_timer()
+    gr17 = aprox_tsp("matrices/gr17.m")
+    res = timeit.default_timer() - start_time
+    print "17 nodes:", res, "seconds"
+
+    start_time = timeit.default_timer()
+    fri26 = aprox_tsp("matrices/fri26.m")
+    res = timeit.default_timer() - start_time
+    print "26 nodes", res, "seconds"
+
+
+
+
+
